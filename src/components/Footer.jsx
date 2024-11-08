91c5f448c22248173cdcdd70f47a168bf60b96c6
Sanskar Singh <163060964+Sanskar1264@users.noreply.github.com>
2024-11-08 18:17:33 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-1 text-gray-600'>
-            <li>+1-212-456-7890</li>
+            <li>+91-755-567-6111</li>
             <li>Contact@foreveryou.com</li>
           </ul>
         </div>
@@ -33,7 +33,7 @@ const Footer = () => {
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com</p>
       </div>
 
     </div>
