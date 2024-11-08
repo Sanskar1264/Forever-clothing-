a5084f00ed09604fe285a661a4abbf1997ffda8c
Sanskar Singh <163060964+Sanskar1264@users.noreply.github.com>
2024-11-08 18:21:08 +0530

    Update NewsletterBox.jsx

--- src/components/NewsletterBox.jsx
+++ src/components/NewsletterBox.jsx
@@ -5,7 +5,7 @@ const NewsletterBox = () => {
     <div className='text-center'>
 
       <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
-      <p className='text-gray-400 mt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+      <p className='text-gray-400 mt-3'>Don’t Just Shop, Be Inspired!" – Subscribe to our newsletter for the latest in Indian fashion and local shopkeepers’ success stories!</p>
 
       <form className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
         <input className='w-full sm:flex-1 outline-none' type="text" placeholder='Enter your email id' required />
