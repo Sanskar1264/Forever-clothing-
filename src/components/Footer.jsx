0fff3494b870d474c2d4fa723320d88f436d2d70
Sanskar Singh <163060964+Sanskar1264@users.noreply.github.com>
2024-11-08 18:10:09 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 
         <div>
           <img className='mb-5 w-32' src={assets.logo} alt="" />
-          <p className='w-full md:w-2/3 text-gray-600'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p className='w-full md:w-2/3 text-gray-600'>"Supporting Local, Wearing Global" – Discover unique designs crafted by Indian artisans. When you shop with us, you help local shopkeepers thrive.</p>
         </div>
 
         <div>
